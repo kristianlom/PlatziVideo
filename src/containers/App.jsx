a64f98cd6e35ecea095d892d415d1e04f883bba1
Kristian Lopez <kristian.lom@gmail.com>
2019-10-03 23:07:53 -0500

    Estrutura y Estilos de Footer.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,6 +5,7 @@ import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
+import Footer from "../components/Footer";
 
 const App = () => (
     <div className="App">
@@ -49,6 +50,7 @@ const App = () => (
                 <CarouselItem/>
             </Carousel>
         </Categories>
+        <Footer/>
     </div>
 );
 
