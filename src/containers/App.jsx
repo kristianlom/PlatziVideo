010efdafbda57aaf49bd40c30f4a517d455fb44c
Kristian Lopez <kristian.lom@gmail.com>
2019-10-04 13:30:02 -0500

    React Hooks: useEffect y useState.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../components/Header";
 import '../assets/styles/App.scss';
 import Search from "../components/Search";
@@ -7,51 +7,65 @@ import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
 
-const App = () => (
-    <div className="App">
-        <Header/>
-        <Search/>
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
-        <Categories title="Originales de Platzi Videos">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
-        <Footer/>
-    </div>
-);
+const App = () => {
+
+    const [videos, setVideos] = useState([]);
+    const API = 'http://localhost:3000/initialState';
+
+    useEffect(() => {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    }, []);
+
+    console.log(videos);
+
+    return (
+        <div className="App">
+            <Header/>
+            <Search/>
+            <Categories title="Mi Lista">
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+            <Categories title="Tendencias">
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+            <Categories title="Originales de Platzi Videos">
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+            <Footer/>
+        </div>
+    );
+};
 
 export default App;
