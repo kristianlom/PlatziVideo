568b4aafa6b5b0e8849a66a1f872cef3f6b5a440
Kristian Lopez <kristian.lom@gmail.com>
2019-10-03 23:35:08 -0500

    Añadiendo imagenes con Webpack

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../assets/styles/components/CarouselItem.scss'
+import play from '../assets/static/play-icon.png';
+import plus from '../assets/static/plus-icon.png';
 
 const CarouselItem = () => (
     <div className="carousel-item">
@@ -8,8 +10,8 @@ const CarouselItem = () => (
              alt=""/>
         <div className="carousel-item__details">
             <div>
-                <img className="carousel-item__details--img" src="../assets/play-icon.png" alt="Play Icon"/>
-                <img className="carousel-item__details--img" src="../assets/plus-icon.png" alt="Plus Icon"/>
+                <img className="carousel-item__details--img" src={play} alt="Play Icon"/>
+                <img className="carousel-item__details--img" src={plus} alt="Plus Icon"/>
             </div>
             <p className="carousel-item__details--title">Título descriptivo</p>
             <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
