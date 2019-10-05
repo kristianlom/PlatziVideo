adfb486e86a628b3ce397bc2dabe8b515afcaadb
Kristian Lopez <kristian.lom@gmail.com>
2019-10-04 22:31:22 -0500

    Creando nuestro archivo de rutas.

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ import useInitialState from "../hooks/useInitialState";
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
 
     const initialState = useInitialState(API);
 
@@ -50,4 +50,4 @@ const App = () => {
     );
 };
 
-export default App;
+export default Home;
