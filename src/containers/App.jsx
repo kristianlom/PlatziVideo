f45e83fca1b9ca4bac96cab563e853a9dd4a8828
Kristian Lopez <kristian.lom@gmail.com>
2019-10-04 20:34:31 -0500

    Custom Hooks.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Header from "../components/Header";
 import '../assets/styles/App.scss';
 import Search from "../components/Search";
@@ -6,24 +6,20 @@ import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
+import useInitialState from "../hooks/useInitialState";
+
+const API = 'http://localhost:3000/initialState';
 
 const App = () => {
 
-    const [videos, setVideos] = useState({mylist: [], trends: [], originals: []});
-    const API = 'http://localhost:3000/initialState';
+    const initialState = useInitialState(API);
 
-    useEffect(() => {
-        fetch(API)
-            .then(response => response.json())
-            .then(data => setVideos(data));
-    }, []);
-
-    return (
+    return initialState.length === 0 ? <h2>Cargando...</h2> : (
         <div className="App">
             <Header/>
             <Search/>
             {
-                videos.mylist.length > 0 &&
+                initialState.mylist.length > 0 &&
                 <Categories title="Mi Lista">
                     <Carousel>
                         <CarouselItem/>
@@ -33,7 +29,7 @@ const App = () => {
             <Categories title="Tendencias">
                 <Carousel>
                     {
-                        videos.trends.map(item =>
+                        initialState.trends.map(item =>
                             <CarouselItem key={item.id} {...item}/>
                         )
                     }
@@ -42,8 +38,8 @@ const App = () => {
             <Categories title="Originales de Platzi Videos">
                 <Carousel>
                     {
-                        videos.originals.length > 0 &&
-                        videos.originals.map(item =>
+                        initialState.originals.length > 0 &&
+                        initialState.originals.map(item =>
                             <CarouselItem key={item.id} {...item}/>
                         )
                     }
