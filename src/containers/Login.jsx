7b3fd4c7cbfe7fd781bbc031a921b9077d497e0c
Kristian Lopez <kristian.lom@gmail.com>
2019-10-04 23:53:41 -0500

    Manejando enlaces y configuraciones.

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import '../assets/styles/components/Login.scss';
 import google from '../assets/static/google-icon.png';
 import twitter from '../assets/static/twitter-icon.png';
@@ -15,14 +16,15 @@ const Login = () => (
                     <label>
                         <input type="checkbox" id="cbox1" value="first_checkbox"/>Recuérdame
                     </label>
-                    <a href="/">Olvidé mi contraseña</a>
+                    <Link to="/">Olvidé mi contraseña</Link>
                 </div>
             </form>
             <section className="login__container--social-media">
                 <div><img src={google} alt="Google Icon"/> Inicia sesión con Google</div>
                 <div><img src={twitter} alt="Twitter Icon"/> Inicia sesión con Twitter</div>
             </section>
-            <p className="login__container--register">No tienes ninguna cuenta <a href="">Regístrate</a></p>
+            <p className="login__container--register">No tienes ninguna cuenta <Link to="/register">Regístrate</Link>
+            </p>
         </section>
     </section>
 );
